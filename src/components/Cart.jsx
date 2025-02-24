a642fbc87a24056dd3ff5253185b4842cf99626d
John Tala <john.tala28@gmail.com>
2025-02-24 10:36:53 +0200

    another iimportant commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -70,7 +70,7 @@ const Cart = () => {
                 <div className="startes" key={food.id}>
                         {food.mymains.map((item) => (
                             <div key={item.id} id='mycontainerCart'>
-                                <img src={item.meal} width="70" height="150" id="myImg"
+                                <img src={item.meal} width="70" height="150" className="myImg"
                                     onClick={() => { handleAddCart(item) }} />
                                 <figcaption>{item.mealdesc}</figcaption>
                                 <figcaption>R {item.mealprice}</figcaption>
@@ -78,7 +78,21 @@ const Cart = () => {
                         )
                         )}
                 </div>
-            )) : <CartItemsPricing
+            )
+                //  <div className="items">
+                //         {foodies.map((food) => (
+                //             <div className="starters" key={food.id}>
+                //                 <div id='mycontainer'>
+                //                    <Link to='/cart'><img src={food.arr[0]} alt={food.alt} id="myImgi" /></Link> 
+                //                  </div>
+                //                 <h3>{food.food}</h3>
+                //                 <div>{food.description.slice(0,40)}</div>
+                //                 <Link to='/cart'><div className='order'><h3>Order now</h3></div></Link>
+                //             </div>
+                //         ))}
+                           
+                //   </div>
+            ) : <CartItemsPricing
                             cart={cart}
                             handleDelete={handleDelete}
                             setShowCart={setShowCart}
