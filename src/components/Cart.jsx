b43d7beaac307adf09eae97771cbc6bfbad4036b
John Tala <john.tala28@gmail.com>
2025-02-24 10:45:49 +0200

    another iimportant commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -79,19 +79,7 @@ const Cart = () => {
                         )}
                 </div>
             )
-                //  <div className="items">
-                //         {foodies.map((food) => (
-                //             <div className="starters" key={food.id}>
-                //                 <div id='mycontainer'>
-                //                    <Link to='/cart'><img src={food.arr[0]} alt={food.alt} id="myImgi" /></Link> 
-                //                  </div>
-                //                 <h3>{food.food}</h3>
-                //                 <div>{food.description.slice(0,40)}</div>
-                //                 <Link to='/cart'><div className='order'><h3>Order now</h3></div></Link>
-                //             </div>
-                //         ))}
-                           
-                //   </div>
+               
             ) : <CartItemsPricing
                             cart={cart}
                             handleDelete={handleDelete}
